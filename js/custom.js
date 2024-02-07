c5e4d53818f650f8d3c73acfde5d5eb489acf3ee
Mario-Marchio <mariomarchio1610@gmail.com>
2024-02-08 00:49:12 +0100

    added footer, img and added some animation

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,28 @@
 // nav menu style
 var nav = $("#navbarSupportedContent");
 var btn = $(".custom_menu-btn");
-btn.click
+btn.click;
 btn.click(function (e) {
-
-    e.preventDefault();
-    nav.toggleClass("lg_nav-toggle");
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+  e.preventDefault();
+  nav.toggleClass("lg_nav-toggle");
+  document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
 });
 
-
 function getCurrentYear() {
-    var d = new Date();
-    var currentYear = d.getFullYear()
+  var d = new Date();
+  var currentYear = d.getFullYear();
 
-    $("#displayDate").html(currentYear);
+  $("#displayDate").html(currentYear);
 }
 
 getCurrentYear();
+
+document.getElementById("button-head").addEventListener("click", function () {
+  // Seleziona l'elemento che deve essere modificato
+  var sliderSection = document.querySelector(".slider_section");
+  if (sliderSection.style.marginTop === "450px") {
+    sliderSection.style.marginTop = "280px";
+  } else {
+    sliderSection.style.marginTop = "450px";
+  }
+});
